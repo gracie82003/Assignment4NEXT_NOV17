e874f04a1bf90006d2485b0fa4e0e48af42ef520
graciegorka <gracie82003@gmail.com>
2023-11-17 17:32:00 -0600

    bb

---
+++ Mymembers/memberspage.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="memberspage.aspx.cs" Inherits="Assignment4NEXT.Mymembers.memberspage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <br />
+    </p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+</asp:Content>
---
+++ Mymembers/memberspage.aspx.cs
@@ -0,0 +1,35 @@
+﻿using Assignment4NEXT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment4NEXT.Mymembers
+{
+    public partial class memberspage : System.Web.UI.Page
+    {
+
+        KarateDataContext dbcon;
+        string conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Graci\\OneDrive\\Desktop\\CSCI 213\\Modual 4\\Assignment4\\NEW\\Assignment4NEXT\\App_Data\\KarateSchool.mdf\";Integrated Security=True;Connect Timeout=30;Encrypt=True";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            dbcon = new KarateDataContext(conn);
+
+            //string myUserName=User.Identity.Name;
+
+            string myUserName = "user1";
+
+           
+            var res=from x in dbcon.NetUsers
+                    select x.UserName;
+
+
+            
+            GridView1.DataSource = res;
+            GridView1.DataBind();
+        }
+    }
+}
+
