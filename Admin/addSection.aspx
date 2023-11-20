304fcc1a45f73b601272948fb808d2b0639fe3fc
graciegorka <gracie82003@gmail.com>
2023-11-20 16:29:38 -0600

    commit 4:20

--- Admin/addSection.aspx
+++ Admin/addSection.aspx
@@ -1,3 +1,36 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="addSection.aspx.cs" Inherits="Assignment4NEXT.Admin.addSection" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        <br />
+    </p>
+    <p>
+        <asp:GridView ID="GridView1" runat="server" DataKeyNames="Member_UserID">
+            <Columns>
+                <asp:CommandField ShowSelectButton="True" />
+            </Columns>
+        </asp:GridView>
+    </p>
+    <p>
+        <asp:GridView ID="GridView2" runat="server" DataKeyNames="InstructorID">
+            <Columns>
+                <asp:CommandField ShowSelectButton="True" />
+            </Columns>
+        </asp:GridView>
+    </p>
+    <p>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Assign to Section" />
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>
+    </p>
 </asp:Content>
--- Admin/addSection.aspx.cs
+++ Admin/addSection.aspx.cs
@@ -9,9 +9,73 @@ namespace Assignment4NEXT.Admin
 {
     public partial class addSection : System.Web.UI.Page
     {
+        string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Graci\\OneDrive\\Desktop\\CSCI 213\\Modual 4\\Assignment4\\NEW\\Assignment4NEXT\\App_Data\\KarateSchool.mdf\";Integrated Security=True;Connect Timeout=30;Encrypt=False";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                // KarateDataContext instance
+                KarateDataContext dataConnection = new KarateDataContext(connectionString);
 
+                // LINQ query
+                var result1 = from item in dataConnection.Members
+                              orderby item.Member_UserID
+                              select new
+                              {
+                                  Member_UserID = item.Member_UserID,
+                                  MemberFirstName = item.MemberFirstName,
+                                  MemberLastName = item.MemberLastName,
+                                  MemberPhoneNumber = item.MemberPhoneNumber,
+                                  MemberDateJoined = item.MemberDateJoined
+                              };
+
+                // Set GridViewView's DataSource propery to result of query and bind
+                GridView1.DataSource = result1;
+                GridView1.DataBind();
+
+                var updatedData = from item in dataConnection.Instructors
+                                  orderby item.InstructorID
+                                  select new
+                                  {
+                                      InstructorID = item.InstructorID,
+                                      InstructorFirstName = item.InstructorFirstName,
+                                      InstructorLastName = item.InstructorLastName
+                                  };
+
+                // Set GridViewView's DataSource propery to result of query and bind
+                GridView2.DataSource = updatedData;
+                GridView2.DataBind();
+            }
+
+
+            }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            KarateDataContext dataConnection = new KarateDataContext(connectionString);
+
+
+            int memberId = Convert.ToInt32(GridView1.SelectedDataKey[0]);
+            int instructorId = Convert.ToInt32(GridView2.SelectedDataKey[0]);
+
+            string sectionName = TextBox1.Text;
+            string start = TextBox2.Text;
+            decimal sectionFee= (Convert.ToDecimal(TextBox3.Text))/100;
+
+            Section section = new Section
+            {
+                SectionName = sectionName,
+                SectionStartDate = Convert.ToDateTime(start),
+                Member_ID= memberId,
+                Instructor_ID= instructorId,
+                SectionFee= sectionFee,
+            };
+
+            dataConnection.Sections.InsertOnSubmit(section);
+
+            // Submit changes to the database
+            dataConnection.SubmitChanges();
         }
     }
 }
